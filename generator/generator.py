e7245253e5530d89bac1a4bfc74c1d9c2d5a065b
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2024-05-30 20:53:50 +0200

    update

--- generator/generator.py
+++ generator/generator.py
@@ -1,5 +1,4 @@
 import os
-import re
 from openai import AzureOpenAI
 
 client = AzureOpenAI(
@@ -10,21 +9,38 @@ client = AzureOpenAI(
 
 deployment = os.getenv("CHAT_COMPLETIONS_DEPLOYMENT_NAME")
 
-with open('temp/body.txt') as f:
-    issue_body = f.read()
-    if "](https://" in issue_body:
-        url = issue_body.split("](")[1].split(")")[0]
-        print(url)
-    else:
-        print("No image found")
-        quit()
+def call_openai(messages):
+    response = client.chat.completions.create(
+        model=deployment,
+        messages = messages,
+        temperature=0.99,
+        max_tokens=800
+    )
+    print(response.choices[0].message.content)
+    return response.choices[0].message
+
+def write_file(file_name, content):
+    with open(file_name, 'w') as f:
+        f.write(content)
+
+def get_url(file):
+    with open(file) as f:
+        issue_body = f.read()
+        if "](https://" in issue_body:
+            url = issue_body.split("](")[1].split(")")[0]
+            return url
+        else:
+            return None
+
+url = get_url('temp/body.txt')
+print(f'URL: {url}
 
 messages=[
-        { "role": "system", "content": "You are an assistant for web developers. You provide working source code based in the requirements of the developers. When they ask you to generate source files your answer must be only the file content, don't add any explanation, instructions or whatever and don't wrap it in markdown, just output the file content." },
+        { "role": "system", "content": "You are an assistant for web developers. You provide working source code based in image sketches. All your answers must be limited to output the generated file content. Don't add any explanation, instruction, or any markdown to the answer. Don't wrap the answer in markdown. Just output the genreated file content." },
         { "role": "user", "content": [  
             { 
                 "type": "text", 
-                "text": "I need the HTML and CSS files to create a web page based in this image. Please, first generate only the HTML file." 
+                "text": "I am creating an Angular project. I have already run the 'ng new' command and know I need the 'index.html', 'styles.css' and 'main.ts' files to create a web page based in this image. Please, first generate only the 'index.html' file." 
             },
             { 
                 "type": "image_url",
@@ -35,25 +51,20 @@ messages=[
         ] } 
     ]
 
-css_message = { "role": "user", "content": "Now please generate only the CSS file." }
+css_message = { "role": "user", "content": "Now please generate only the 'styles.css' file." }
+ts_message = { "role": "user", "content": "And finally please generate only the 'main.ts' file." }
 
-response = client.chat.completions.create(
-        model=deployment,
-        messages = messages,
-        temperature=0.99,
-        max_tokens=800
-    )
+response = call_openai(messages)
+write_file('temp/index.html', response.content)
 
-print(response.choices[0].message.content)
-
-messages.append(response.choices[0].message)
+messages.append(response)
 messages.append(css_message)
 
-response = client.chat.completions.create(
-        model=deployment,
-        messages = messages,
-        temperature=0.99,
-        max_tokens=800
-    )
+response = call_openai(messages)
+write_file('temp/styles.css', response.content)
 
-print(response.choices[0].message.content)
+messages.append(response)
+messages.append(ts_message)
+
+response = call_openai(messages)
+write_file('temp/main.ts', response.content)
