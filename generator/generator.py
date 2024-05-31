07cd118729af324299a5c34bee976ca0553469d2
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2024-05-31 13:02:04 +0200

    update

--- generator/generator.py
+++ generator/generator.py
@@ -13,7 +13,7 @@ def call_openai(messages):
     response = client.chat.completions.create(
         model=deployment,
         messages = messages,
-        temperature=0.99,
+        temperature=0.1,
         max_tokens=800
     )
     print(response.choices[0].message.content)
@@ -36,11 +36,11 @@ url = get_url('temp/body.txt')
 print(url)
 
 messages=[
-        { "role": "system", "content": "You are an assistant for web developers. You provide working source code based in image sketches. All your answers must be limited to output the generated file content. Don't add any explanation, instruction, or any markdown to the answer. Don't wrap the answer in markdown. Just output the genreated file content." },
+        { "role": "system", "content": "You are an assistant for web developers. You provide working source code based in image sketches. All your answers must be limited to output the generated file content. Don't add any explanation, instruction, or any markdown to the answer. Don't wrap the answer in markdown. Just output the generated file content." },
         { "role": "user", "content": [  
             { 
                 "type": "text", 
-                "text": "I am creating an Angular project. I have already run the 'ng new' command and know I need the 'index.html', 'styles.css' and 'main.ts' files to create a web page based in this image. Please, first generate only the 'index.html' file." 
+                "text": "I am creating an Angular project. I have already run the 'ng new' command and now I need the 'index.html', 'styles.css' and 'main.ts' files to create a web page based in this image. Please, first generate only the 'index.html' file." 
             },
             { 
                 "type": "image_url",
@@ -51,8 +51,9 @@ messages=[
         ] } 
     ]
 
-css_message = { "role": "user", "content": "Now please generate only the 'styles.css' file." }
-ts_message = { "role": "user", "content": "And finally please generate only the 'main.ts' file." }
+css_message = { "role": "user", "content": "Now generate only the 'styles.css' file." }
+ts_message = { "role": "user", "content": "And finally generate only the 'main.ts' file." }
+summary_message = { "role": "user", "content": "Now generate a summary in markdown of what has been done so a developer can understand the files generated." }
 
 response = call_openai(messages)
 write_file('temp/index.html', response.content)
@@ -68,3 +69,9 @@ messages.append(ts_message)
 
 response = call_openai(messages)
 write_file('temp/main.ts', response.content)
+
+messages.append(response)
+messages.append(summary_message)
+
+response = call_openai(messages)
+write_file('temp/summary.md', response.content)
